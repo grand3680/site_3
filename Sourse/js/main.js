2ef90fd35bbbe05904106d7165a00c7d82da921e
beginner <84720129+grand3680@users.noreply.github.com>
2022-10-30 16:01:53 +0300

    Add files via upload

---
+++ Sourse/js/main.js
@@ -0,0 +1,7 @@
+let menu_content = document.getElementById("menu_content");
+
+
+function menu() {
+    menu_content.classList.toggle('ative');
+    document.querySelector("body").classList.toggle('overflow');;
+}
